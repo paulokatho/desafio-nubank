0fcb7e9f38f8590824360ebbc3096cfc0d7fee61
paulokatho <pkatho@gmail.com>
2025-06-21 17:12:02 +0100

    feat: criando serviços, configuraçoes e conexao com BD basicos.

---
+++ nubank/src/main/java/spring/boot/desafio/nubank/NubankApplication.java
@@ -0,0 +1,14 @@
+package spring.boot.desafio.nubank;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubankApplication.class, args);
+	}
+
+}
